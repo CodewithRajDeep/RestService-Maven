03b030a018b68f4edaa975cb96acdf15a8b6d0e6
CodewithRajDeep <deepraj6897@gmail.com>
2024-09-10 20:33:44 +0530

    SpringBoot rest service

---
+++ HelloRest/src/main/java/com/example/ws/HelloController.java
@@ -0,0 +1,10 @@
+package com.example.ws;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+@RestController
+public class HelloController {
+@RequestMapping("/hello")
+public String hello() {
+	return "Hello World";
+}
+}
